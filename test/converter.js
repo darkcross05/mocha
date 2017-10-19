03f77d68d8509262464694c858aabe4848b35bef
darkcross05 <juanpa96@live.com.mx>
2017-10-19 15:51:22 -0500

    Third commit

--- test/converter.js
+++ test/converter.js
@@ -1,4 +1,4 @@
-var today    = require("chai").today;
+var expect    = require("chai").expect;
 var converter = require("../app/converter");
 
 describe("Color Code Converter", function(){
@@ -8,9 +8,9 @@ describe("Color Code Converter", function(){
       var greenHex = converter.rgbToHex(0, 255, 0);
       var blueHex = converter.rgbToHex(0, 0, 255);
 
-      today(redHex).to.deep.equal("ff0000");
-      today(greenHex).to.deep.equal("00ff00");
-      today(blueHex).to.deep.equal("0000ff");
+      expect(redHex).to.deep.equal("ff0000");
+      expect(greenHex).to.deep.equal("00ff00");
+      expect(blueHex).to.deep.equal("0000ff");
     });
   });
 
@@ -19,10 +19,10 @@ describe("Color Code Converter", function(){
       var red = converter.hexToRgb("ff0000");
       var green = converter.hexToRgb("00ff00");
       var blue = converter.hexToRgb("0000ff");
-}
-      today(red).to.deep.equal([255, 0, 0]);
-      today(green).to.deep.equal([0, 255, 0]);
-      today(blue).to.deep.equal([0, 0, 255]);
+
+      expect(red).to.deep.equal([255, 0, 0]);
+      expect(green).to.deep.equal([0, 255, 0]);
+      expect(blue).to.deep.equal([0, 0, 255]);
     });
   });
 });
